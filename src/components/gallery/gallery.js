a6a2431523c3ce4fce933f230df44aacafbbfdb0
Rubén Fernández <rubenferbu@gmail.com>
2026-06-27 08:31:20 +0200

    Fix import path for createImageCard function

--- src/components/gallery/gallery.js
+++ src/components/gallery/gallery.js
@@ -2,7 +2,7 @@
 // COMPONENTE: Gallery (galería de imágenes)
 // -----------------------------------------
 
-import { createImageCard } from "../ImageCard/imageCard.js";
+import { createImageCard } from "../imageCard/imageCard.js";
 
 /**
  * Crea la galería completa a partir de un array de imágenes
